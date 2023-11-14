6cbd817d3da782a7491985f1914aea04fc525e01
teklitg <teklit.gebrer@gmail.com>
2023-11-14 01:24:45 -0800

    hkj

--- myApp.js
+++ myApp.js
@@ -1,48 +1,37 @@
-let express = require('express');
-require('dotenv').config()
+require('dotenv').config();
+var express = require('express');
+var app = express();
+const mySecret = process.env['MESSAGE_STYLE']
 
-let app = express();
+app.use('/public', express.static(__dirname + '/public'));
 
-
-console.log("Hello World")
-
-app.use("/public", express.static(__dirname + "/public"))
-
-app.get("/", (req, res)=>{
-    res.sendFile(__dirname + "/views/index.html")
-})
-
-
-app.get("/json", (req, res)=>{
-    if (process.env.MESSAGE_STYLE === "uppercase") {
-         res.json({"message": "HELLO JSON"})
-    } else {
-        res.json({"message": "Hello json"})
-    }
-})
+app.get('/', function(req, res){
+  console.log("Hello world");
+  res.sendFile(__dirname + '/views/index.html');
+});
 
 
 
+app.get('/json', (req, res) => {
+  if (process.env.MESSAGE_STYLE==='uppercase') {
+    return res.json({"message": "Hello json"}).toUpperCase();
+  } else {
+    return res.json({"message": "Hello json"});
+  }
+});
 
+console.log('End JSON get method');
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// app.get("/json", (req, res) => {
+//   let message = {"message": "Hello json"};
+  
+//   if( process.env.MESSAGE_STYLE === 'uppercase'){
+//       return message['message'] = message['message'].toUpperCase();
+//       console.log(message['message']);
+//   } else {
+//       return res.json(message);
+//   }
+// });
 
 
  module.exports = app;
